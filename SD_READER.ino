8bf272d785d9d021cbe8345639c12ee6418e342f
Shaun Price <shaun@priceconsulting.biz>
2020-12-23 03:23:45 +1100

    Updated for Teensy 3.5+

--- SD_READER.ino
+++ SD_READER.ino
@@ -21,93 +21,48 @@
 	4. Check FSEQ header. channelsPerStep doesn't always match 
 
 	*/
-#include <SD.h>
+
+#include <sdios.h>
+#include <SdFatConfig.h>
+#include <SdFat.h>
+#include <FSEQLib.h>
 #include <SPI.h>
 
 #define CardDetectedTrue  1
 #define CardDetectedFalse 0
 
+#ifdef ESP32
 SPIClass spiFPGA(HSPI);
 #define FPGA_HANDSHAKE 2
 #define FPGA_MISO 12
 #define FPGA_MOSI 13
 #define FPGA_SCK  14
 #define FPGA_CS   15
-
 const int cardDetectedPin = GPIO_NUM_17; // May require 10k pull-up
-const int UNIVERSES = 10;
-const int PIXELS = 144;
-const int LEDS_PER_UNIVERSE = PIXELS * 3;
-const int BUFFER_LENGTH = (UNIVERSES * LEDS_PER_UNIVERSE) + ((4 - (UNIVERSES * LEDS_PER_UNIVERSE) % 4) % 4);
+#else
+const uint8_t chipSelect = BUILTIN_SDCARD; // Teensy 3.5 & 3.6 & 4.1 on-board
+#define SD_FAT_TYPE 3
+#endif
+
+#define FPGA_CS   15
+
+const size_t UNIVERSES = 16 ;
+const size_t PIXELS = 150;
+const size_t LEDS_PER_UNIVERSE = PIXELS * 3;
+const size_t BUFFER_LENGTH = (UNIVERSES * LEDS_PER_UNIVERSE) + ((4 - (UNIVERSES * LEDS_PER_UNIVERSE) % 4) % 4);
 size_t bytesRead = 0;
-File dataFile;
+SdFs sd;
+FsFile dataFile;
+HeaderData rawHeader;
+FSEQLib header;
+const  char* filename = "Rainbow.fseq";
 
 // SPI
 char stepBuffer[BUFFER_LENGTH];
 uint8_t universeBuffer[LEDS_PER_UNIVERSE + 1];
 char * receiveFPGABuffer;
 
-int32_t currentStep = 0;
-
-#pragma pack(push, 1)
-typedef struct headerData_t
-{
-	char	  magic[4];	// Should be "PSEQ"
-	uint16_t  dataOffset;
-	uint8_t   minorVersion;
-	uint8_t   majorVersion;
-	uint16_t  headerLength;
-	uint16_t  lchannelsPerStep;
-	uint16_t  hchannelsPerStep;
-	uint16_t  lstepLength;
-	uint16_t  hstepLength;
-	uint16_t  fifty; // Always 50
-	uint16_t  universes;
-	uint16_t  sizeofUniverse;
-	uint8_t   gamma;
-	uint8_t   lightType;
-};
-#pragma pop() 
-
-typedef union HeaderData
-{
-	headerData_t headerData;
-	char rawData[28];	    // Xlights FSEQ Header
-};
-
-class XlightsHeader
-{
-private:
-	headerData_t _header;
-public:
-	XlightsHeader() {};
-
-	XlightsHeader(HeaderData header)
-	{
-		_header = header.headerData;
-	}
-
-	void xlightsHeader(HeaderData header)
-	{
-		_header = header.headerData;
-	}
-
-	String magic() { return String(_header.magic); }
-	uint16_t dataOffset() { return _header.dataOffset; }
-	String versionString() { return String(_header.majorVersion) + "." + String(_header.minorVersion); }
-	uint8_t majorVersion() { return _header.majorVersion; }
-	uint8_t minorVersion() { return _header.minorVersion; }
-	uint16_t headerLength() { return (int)_header.headerLength; }
-	uint32_t channelsPerStep() { return (uint32_t)_header.lchannelsPerStep + (uint32_t)(_header.hchannelsPerStep * 65536); }
-	uint32_t stepLength() { return (uint32_t)_header.lstepLength + (uint32_t)(_header.hstepLength * 65536); }
-	uint16_t universes() { return _header.universes; }
-	uint16_t sizeofUniverses() { return _header.sizeofUniverse; }
-	uint8_t gamma() { return _header.gamma; }
-	String lightType() { return (_header.lightType == 0) ? "RGB" : "UNKNOWN"; }
-};
-
-HeaderData rawHeader;
-XlightsHeader header;
+uint32_t currentStep = 0;
 
 bool cardInitialised = false;
 bool cardDetected = false;
@@ -116,7 +71,9 @@ void setup()
 {
 	Serial.begin(115200);
 
+#ifdef ESP32	
 	pinMode(SS, OUTPUT); // SD Card VSPI SS
+
 	pinMode(cardDetectedPin,INPUT_PULLUP); // SD Card Detected CD 
 
 	// FPGA PIN Mode
@@ -126,58 +83,87 @@ void setup()
 	pinMode(FPGA_CS, OUTPUT);
 
 	spiFPGA.begin(FPGA_SCK, FPGA_MISO, FPGA_MOSI, FPGA_CS);
-
+#else
+	SPI.begin();
+#endif
 	digitalWrite(FPGA_CS, 1);
 }
 
 void loop()
 {
-	unsigned long startMillis = millis();
+	unsigned long startMillis = millis();	
+	
+	Serial.println("Checking Initialization state of SD card...");
+#ifdef ESP32
 	cardDetected = (digitalRead(cardDetectedPin) == 1) ? true : false;
+
 	// Initialise SPI Master
 
 	// See if the card is present and can be initialized:
 	if (cardDetected && !cardInitialised)
 	{
-		Serial.print("Initializing SD card...");
-		
+		Serial.println("Initializing SD card...");
 		if (SD.begin())
 		{
-			Serial.println("card initialized.");
-
-			// open the file. note that only one file can be open at a time,
-			// so you have to close this one before opening another.
-			dataFile = SD.open("/xlight00.dat");
-			Serial.println("File size: " + String(dataFile.size()));
-			if (dataFile.size() > 0)
+#else
+	if (!cardInitialised)
+	{		
+		Serial.println("Initializing SD card...");
+		if (sd.begin(chipSelect))
+		{
+#endif				
+			Serial.println("SD card initialized.");
+			Serial.println("Checking for Xlights FSEQ file.");
+			// Find the FSEQ file
+			sd.ls();
+			if (sd.exists(filename))
 			{
-				cardInitialised = true;
+				Serial.println("FSEQ File Exists. Reading file.");
+				dataFile = sd.open(filename);
+				Serial.print("File size: ");
+				dataFile.printFileSize(&Serial);
+				Serial.println(" bytes");
 
-				dataFile.readBytes(rawHeader.rawData, 28);
+				if (dataFile.size() > 0)
+				{
+					dataFile.readBytes(rawHeader.rawData, 28);
+					header = FSEQLib(rawHeader);
 
-				header = XlightsHeader(rawHeader);
+					Serial.println("======================");
+					Serial.println("== Xlights FSEQ Header");
+					Serial.println("======================");
+					Serial.println("Magic: " + header.magic());
+					Serial.println("Data Offset: " + String(header.dataOffset()));
+					Serial.println("Version: " + header.majorVersion());
+					Serial.println("Header Length: " + String(header.headerLength()));
+					Serial.println("Channels per Step: " + String(header.channelsPerStep()));
+					Serial.println("Step Length: " + String(header.stepLength()));
+					Serial.println("Universes: " + String((header.universes() == 0) ? "N/A" : String(header.universes())));
+					Serial.println("Size of Universe: " + String((header.sizeofUniverses() == 0) ? "N/A" : String(header.sizeofUniverses())));
+					Serial.println("Gamma: " + String(header.gamma()));
+					Serial.println("Light Type: " + header.lightType());
+					Serial.println("======================");
 
-				Serial.println("======================");
-				Serial.println("== Xlights FSEQ Header");
-				Serial.println("======================");
-				Serial.println("Magic: " + header.magic());
-				Serial.println("Data Offset: " + String(header.dataOffset()));
-				Serial.println("Version: " + header.versionString());
-				Serial.println("Header Length: " + String(header.headerLength()));
-				Serial.println("Channels per Step: " + String(header.channelsPerStep()));
-				Serial.println("Step Length L: " + String(header.stepLength()));
-				Serial.println("Universes: " + String((header.universes() == 0) ? "N/A" : String(header.universes())));
-				Serial.println("Size of Universe: " + String((header.sizeofUniverses() == 0) ? "N/A" : String(header.sizeofUniverses())));
-				Serial.println("Gamma: " + String(header.gamma()));
-				Serial.println("Light Type: " + (header.lightType() == 0) ? "RGB" : "Unknown");
-				Serial.println("======================");
-
-				Serial.println("done!");
+					// Set the initialised state if the magic is correct
+					if (header.magic() == "PSEQ")
+					{
+						cardInitialised = true;
+						Serial.println("done!");
+					}
+					else
+					{
+						Serial.println("Invalid Magic in header.");
+					}
+				}
+				else
+				{
+					cardInitialised = false;
+					dataFile.close();
+				}
 			}
 			else
 			{
-				cardInitialised = false;
-				dataFile.close();
+				Serial.println("File does not exist.");
 			}
 		}
 		else
@@ -186,22 +172,31 @@ void loop()
 			delay(500);
 		}
 	}
+#ifdef ESP32
 	else if (!cardDetected && cardInitialised)
 	{
 		Serial.println("Card removed"); 
 		cardInitialised = false;
 		dataFile.close();
 		SD.end();
+
 	}
 	else if (cardDetected && cardInitialised)
-	{ 
+#else
+	else
+#endif
+	{
+		Serial.println("Card initialized. Reading data.");
+
 		// Read the channels for the next step
 		if (dataFile.readBytes(stepBuffer, BUFFER_LENGTH) != BUFFER_LENGTH)
 		{
 			Serial.println("Buffer Failed to load. Closing SD Card");
 			cardInitialised = false;
 			dataFile.close();
+#ifdef ESP32
 			SD.end();
+#endif		
 		}
 
 		// Output data
@@ -217,10 +212,17 @@ void loop()
 			digitalWrite(FPGA_CS, 0);
 
 			// Send the data
+#ifdef ESP32
 			spiFPGA.writeBytes(&universeBuffer[0], LEDS_PER_UNIVERSE + 1);
-			
+#else
+			SPI.transfer(&universeBuffer[0], LEDS_PER_UNIVERSE + 1);
+#endif
+
 			// Raise the CS line
 			digitalWrite(FPGA_CS, 1);
+#ifndef ESP32
+			SPI.endTransaction();
+#endif
 		}
 		
 		currentStep++;
@@ -236,5 +238,5 @@ void loop()
 	}
 	// Delay to make sure we send x20 per second.
 	// The delay is constrained between 1ms and 50ms
-	delay(constrain(50 - (millis() - startMillis),1,50));
+	delay(constrain(50 - (millis() - (uint32_t)startMillis),1,50));
 }
--- __vm/.SD_READER.vsarduino.h
+++ __vm/.SD_READER.vsarduino.h
@@ -1,110 +1,99 @@
 /* 
 	Editor: https://www.visualmicro.com/
-			visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
-			the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
-			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
-			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+			This file is for intellisense purpose only.
+			Visual micro (and the arduino ide) ignore this code during compilation. This code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+			The contents of the _vm sub folder can be deleted prior to publishing a project
+			All non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+			Note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: WEMOS LOLIN32, Platform=esp32, Package=esp32
+	Hardware: Teensy 3.5, Platform=teensy3, Package=teensy
 */
 
 #if defined(_VMICRO_INTELLISENSE)
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __ESP32_ESp32__
-#define __ESP32_ESP32__
-#define ESP_PLATFORM
-#define HAVE_CONFIG_H
-#define F_CPU 240000000L
-#define ARDUINO 10805
-#define ARDUINO_LOLIN32
-#define ARDUINO_ARCH_ESP32
-#define ESP32
-#define CORE_DEBUG_LEVEL 0
+#define __HARDWARE_mk64fx512__
+#define __HARDWARE_MK64FX512__
+#define _VMDEBUG 1
+#define __MK64FX512__
+#define TEENSYDUINO 153
+#define ARDUINO 108013
+#define ARDUINO_TEENSY35
+#define F_CPU 120000000
+#define USB_SERIAL
+#define LAYOUT_US_ENGLISH
 #define __cplusplus 201103L
 #undef __cplusplus
 #define __cplusplus 201103L
-#define __STDC__
-#define __ARM__
+
+
 #define __arm__
-#define __inline__
-#define __asm__(x)
-#define __asm__
+#define __ARM__
+#define  __attribute__(x)
+typedef void *__builtin_va_list;
 #define __extension__
 #define __ATTR_PURE__
 #define __ATTR_CONST__
+#define __inline__
+#define __asm__(x)
 #define __volatile__
+#define NEW_H
+#undef _WIN32
+#define __STDC__ 
+//#define __GNUC__ 2
+//#define __GNUC_MINOR__ 5
+#define __ARM_ARCH_7EM__
 
-
-#define __ASM
-#define __INLINE
-#define __attribute__(noinline)
-
-//#define _STD_BEGIN
-//#define EMIT
-#define WARNING
-#define _Lockit
-#define __CLR_OR_THIS_CALL
-#define C4005
-#define _NEW
-
-//
-//typedef int uint8_t;
-//#define __ARMCC_VERSION 400678
-//#define PROGMEM
-//#define string_literal
-//
-//#define prog_void
-//#define PGM_VOID_P int
-//
-
-typedef int _read;
-typedef int _seek;
-typedef int _write;
-typedef int _close;
-typedef int __cleanup;
-
-//#define inline 
-
-#define __builtin_clz
-#define __builtin_clzl
-#define __builtin_clzll
-#define __builtin_labs
-#define __builtin_va_list
-typedef int __gnuc_va_list;
-
-#define __ATOMIC_ACQ_REL
-
-#define __CHAR_BIT__
-#define _EXFUN()
-
-typedef unsigned char byte;
-extern "C" void __cxa_pure_virtual() {;}
-
-
-typedef long __INTPTR_TYPE__ ;
-typedef long __UINTPTR_TYPE__ ;
-typedef long __SIZE_TYPE__ 	;
-typedef long __PTRDIFF_TYPE__;
+extern int at_quick_exit(void (*f)(void));
+int at_quick_exit(void (*f)(void)) {
+}
+extern int quick_exit(void (*f)(void));
+int quick_exit(void (*f)(void)) {
+}
 
 
 
+#define __INT64_TYPE__ 8
+#define __INTPTR_TYPE__ 4
+#define __INT32_TYPE__ 4
 
-#include <arduino.h>
-#include <pins_arduino.h> 
+typedef long intptr_t;
+typedef long __intptr_t;
+typedef unsigned long __uintptr_t;
+typedef long __int32_t;
+typedef unsigned long __uint32_t;
+typedef unsigned short  __uint16_t;
+typedef short __int16_t;
+typedef unsigned short  __uint8_t;
+typedef short __int8_t;
+typedef unsigned long __uint64_t;
+typedef double __int64_t;
+typedef unsigned long uint64_t;
+typedef double int64_t;
+typedef unsigned short uint8_t;
+typedef short int8_t;
 
-//#include "..\generic\Common.h"
-//#include "..\generic\pins_arduino.h"
+typedef unsigned int uint16_t;
+typedef short int16_t;
+typedef long __int32_t;
+typedef unsigned long __uint32_t;
 
-//#undef F
-//#define F(string_literal) ((const PROGMEM char *)(string_literal))
-//#undef PSTR
-//#define PSTR(string_literal) ((const PROGMEM char *)(string_literal))
-//current vc++ does not understand this syntax so use older arduino example for intellisense
-//todo:move to the new clang/gcc project types.
-#define interrupts() sei()
-#define noInterrupts() cli()
+#define at_quick_exit(x)
+
+#include "arduino.h"
+#define abs(x) ((x)>0?(x):-(x))
+#define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+#define round(x)     ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
+#define radians(deg) ((deg)*DEG_TO_RAD)
+#define degrees(rad) ((rad)*RAD_TO_DEG)
+#define sq(x) ((x)*(x))
+
+#define __asm__
+
+#define __disable_irq() __asm__ volatile("");
+#define __enable_irq()	__asm__ volatile("");
+
 
 #include "SD_READER.ino"
 #endif

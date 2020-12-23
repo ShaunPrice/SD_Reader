f95c59e3797b2251efa8bb1419bcfc64ebd5dc7e
Shaun Price <shaun@priceconsulting.biz>
2020-12-23 23:23:33 +1100

    Update SD_READER.ino
    Added Teensy 3.5 support

--- SD_READER.ino
+++ SD_READER.ino
@@ -8,9 +8,13 @@
 	========================================
 	SCLK = 18, MISO = 19, MOSI = 23, SS = 5
 
-	FPGA Card HSPI PINS
+	FPGA SPI ESP32 HSPI PINS
 	========================================
 	SCLK = 14, MISO = 12, MOSI = 13, SS = 15
+
+	FPGA SPI Teensy Pins
+	========================================
+	SCLK = 13, MISO = 12, MOSI = 11, SS = 10
 
 	========================================
 	TODO
@@ -28,6 +32,17 @@
 #include <FSEQLib.h>
 #include <SPI.h>
 
+//#define DEBUG
+
+
+#ifdef DEBUG
+#define DEBUG_PRINTLN(x)  DEBUG_PRINT(x); Serial.println (x);
+#define DEBUG_PRINT(x); Serial.print (x);
+#else
+#define DEBUG_PRINTLN(x)
+#define DEBUG_PRINT(x)
+#endif
+
 #define CardDetectedTrue  1
 #define CardDetectedFalse 0
 
@@ -42,23 +57,24 @@ const int cardDetectedPin = GPIO_NUM_17; // May require 10k pull-up
 #else
 const uint8_t chipSelect = BUILTIN_SDCARD; // Teensy 3.5 & 3.6 & 4.1 on-board
 #define SD_FAT_TYPE 3
+#define FPGA_CS   10
 #endif
 
-#define FPGA_CS   15
+
 
 const size_t UNIVERSES = 16 ;
 const size_t PIXELS = 150;
 const size_t LEDS_PER_UNIVERSE = PIXELS * 3;
-const size_t BUFFER_LENGTH = (UNIVERSES * LEDS_PER_UNIVERSE) + ((4 - (UNIVERSES * LEDS_PER_UNIVERSE) % 4) % 4);
+const size_t STEP_BUFFER_LENGTH = (UNIVERSES * LEDS_PER_UNIVERSE) + ((4 - (UNIVERSES * LEDS_PER_UNIVERSE) % 4) % 4);
 size_t bytesRead = 0;
 SdFs sd;
 FsFile dataFile;
 HeaderData rawHeader;
 FSEQLib header;
-const  char* filename = "Rainbow.fseq";
+const  char* filename = "data.fseq";
 
 // SPI
-char stepBuffer[BUFFER_LENGTH];
+char stepBuffer[STEP_BUFFER_LENGTH];
 uint8_t universeBuffer[LEDS_PER_UNIVERSE + 1];
 char * receiveFPGABuffer;
 
@@ -69,7 +85,10 @@ bool cardDetected = false;
 
 void setup()
 {
+#ifdef DEBUG
 	Serial.begin(115200);
+#endif
+	pinMode(FPGA_CS, OUTPUT);
 
 #ifdef ESP32	
 	pinMode(SS, OUTPUT); // SD Card VSPI SS
@@ -80,20 +99,19 @@ void setup()
 	pinMode(FPGA_MOSI, OUTPUT);
 	pinMode(FPGA_MISO, INPUT);
 	pinMode(FPGA_SCK, OUTPUT);
-	pinMode(FPGA_CS, OUTPUT);
 
 	spiFPGA.begin(FPGA_SCK, FPGA_MISO, FPGA_MOSI, FPGA_CS);
 #else
 	SPI.begin();
 #endif
-	digitalWrite(FPGA_CS, 1);
+	digitalWrite(FPGA_CS, HIGH);
 }
 
 void loop()
 {
 	unsigned long startMillis = millis();	
 	
-	Serial.println("Checking Initialization state of SD card...");
+	DEBUG_PRINTLN("Checking Initialization state of SD card...");
 #ifdef ESP32
 	cardDetected = (digitalRead(cardDetectedPin) == 1) ? true : false;
 
@@ -102,57 +120,53 @@ void loop()
 	// See if the card is present and can be initialized:
 	if (cardDetected && !cardInitialised)
 	{
-		Serial.println("Initializing SD card...");
+		DEBUG_PRINT("Initializing SD card...");
 		if (SD.begin())
 		{
 #else
 	if (!cardInitialised)
 	{		
-		Serial.println("Initializing SD card...");
+		DEBUG_PRINTLN("Initializing SD card...");
 		if (sd.begin(chipSelect))
 		{
 #endif				
-			Serial.println("SD card initialized.");
-			Serial.println("Checking for Xlights FSEQ file.");
+			DEBUG_PRINTLN("SD card initialized.");
+			DEBUG_PRINTLN("Checking for Xlights FSEQ file.");
 			// Find the FSEQ file
 			sd.ls();
 			if (sd.exists(filename))
 			{
-				Serial.println("FSEQ File Exists. Reading file.");
+				DEBUG_PRINTLN("FSEQ File Exists. Reading file.");
 				dataFile = sd.open(filename);
 				Serial.print("File size: ");
 				dataFile.printFileSize(&Serial);
-				Serial.println(" bytes");
+				DEBUG_PRINTLN(" bytes");
 
 				if (dataFile.size() > 0)
 				{
 					dataFile.readBytes(rawHeader.rawData, 28);
 					header = FSEQLib(rawHeader);
 
-					Serial.println("======================");
-					Serial.println("== Xlights FSEQ Header");
-					Serial.println("======================");
-					Serial.println("Magic: " + header.magic());
-					Serial.println("Data Offset: " + String(header.dataOffset()));
-					Serial.println("Version: " + header.majorVersion());
-					Serial.println("Header Length: " + String(header.headerLength()));
-					Serial.println("Channels per Step: " + String(header.channelsPerStep()));
-					Serial.println("Step Length: " + String(header.stepLength()));
-					Serial.println("Universes: " + String((header.universes() == 0) ? "N/A" : String(header.universes())));
-					Serial.println("Size of Universe: " + String((header.sizeofUniverses() == 0) ? "N/A" : String(header.sizeofUniverses())));
-					Serial.println("Gamma: " + String(header.gamma()));
-					Serial.println("Light Type: " + header.lightType());
-					Serial.println("======================");
+					DEBUG_PRINTLN("======================");
+					DEBUG_PRINTLN("== Xlights FSEQ Header");
+					DEBUG_PRINTLN("======================");
+					DEBUG_PRINTLN("Magic: " + header.magic());
+					DEBUG_PRINTLN("Data Offset: " + String(header.dataOffset()));
+					DEBUG_PRINTLN("Version: " + header.majorVersion());
+					DEBUG_PRINTLN("Header Length: " + String(header.headerLength()));
+					DEBUG_PRINTLN("Channels per Step: " + String(header.channelsPerStep()));
+					DEBUG_PRINTLN("Step Length: " + String(header.sequenseSteps()));
+					DEBUG_PRINTLN("======================");
 
 					// Set the initialised state if the magic is correct
 					if (header.magic() == "PSEQ")
 					{
 						cardInitialised = true;
-						Serial.println("done!");
+						DEBUG_PRINTLN("done!");
 					}
 					else
 					{
-						Serial.println("Invalid Magic in header.");
+						DEBUG_PRINTLN("Invalid Magic in header.");
 					}
 				}
 				else
@@ -163,19 +177,19 @@ void loop()
 			}
 			else
 			{
-				Serial.println("File does not exist.");
+				DEBUG_PRINTLN("File does not exist.");
 			}
 		}
 		else
 		{
-			Serial.println("Card failed, or not present");
+			DEBUG_PRINTLN("Card failed, or not present");
 			delay(500);
 		}
 	}
 #ifdef ESP32
 	else if (!cardDetected && cardInitialised)
 	{
-		Serial.println("Card removed"); 
+		DEBUG_PRINT("Card removed"); 
 		cardInitialised = false;
 		dataFile.close();
 		SD.end();
@@ -186,12 +200,12 @@ void loop()
 	else
 #endif
 	{
-		Serial.println("Card initialized. Reading data.");
+		DEBUG_PRINTLN("Card initialized. Reading data.");
 
 		// Read the channels for the next step
-		if (dataFile.readBytes(stepBuffer, BUFFER_LENGTH) != BUFFER_LENGTH)
+		if (dataFile.readBytes(stepBuffer, STEP_BUFFER_LENGTH) != STEP_BUFFER_LENGTH)
 		{
-			Serial.println("Buffer Failed to load. Closing SD Card");
+			DEBUG_PRINTLN("Buffer Failed to load. Closing SD Card");
 			cardInitialised = false;
 			dataFile.close();
 #ifdef ESP32
@@ -202,33 +216,35 @@ void loop()
 		// Output data
 		for (uint8_t current_universe = 0; current_universe < UNIVERSES; current_universe++)
 		{
-			// Set the one byte header to the universe number (starting with 0)
-			universeBuffer[0] = current_universe;
-			
 			// Copy the led values into the universe buffer
 			memcpy(&universeBuffer[1],&stepBuffer[current_universe * LEDS_PER_UNIVERSE], LEDS_PER_UNIVERSE);
-			
+
+			// Set the one byte header to the universe number (starting with 0)
+			universeBuffer[0] = current_universe;
+
+			SPI.beginTransaction(SPISettings(4000000, MSBFIRST, SPI_MODE0));
+
 			// Lower the CS line
-			digitalWrite(FPGA_CS, 0);
+			digitalWrite(FPGA_CS, LOW);
 
 			// Send the data
 #ifdef ESP32
 			spiFPGA.writeBytes(&universeBuffer[0], LEDS_PER_UNIVERSE + 1);
 #else
 			SPI.transfer(&universeBuffer[0], LEDS_PER_UNIVERSE + 1);
-#endif
-
+#endif		
 			// Raise the CS line
-			digitalWrite(FPGA_CS, 1);
-#ifndef ESP32
+			digitalWrite(FPGA_CS, HIGH);
+
 			SPI.endTransaction();
-#endif
+
+			DEBUG_PRINTLN("Universe "+String(current_universe)+" send complete.");
 		}
 		
 		currentStep++;
 
 		// Reset to first step if we have gone past the last step
-		if (currentStep == header.stepLength())
+		if (currentStep == header.sequenseSteps())
 		{
 			// Restart at first step
 			currentStep = 0;
